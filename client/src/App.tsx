a7f8831cf1a9740d5a0cbb3ecc20e388cade4e35
Joshua Cutts <josh.cutts1@gmail.com>
2025-05-10 12:28:32 -0700

    NewTub component created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,30 @@
 import { useState } from 'react'
-import tubImage from './assets/tub.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// new tub component
+  // 
+function NewTub() {
+  return (
+    <div>
+      <h1>New Tubs</h1>
+      <p>Create a new tub to collect and inspect HTTP requests</p>
+      <form>
+        <div>
+          <label><span>https://rbaskets.in/</span></label>
+          <input type="text" placeholder="type a name"/>
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  )
+}
 
+// my tubs component
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://www.youtube.com/watch?v=kvxCU_lQwKM" target="_blank">
-        <img src={tubImage} alt="Funny bathtub" style={{ width: '400px', height: 'auto' }} />
-        </a>
-      </div>
-      <h1>Team Tub</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          tub count is {count}
-        </button>
-      </div>
+      <NewTub />
     </>
   )
 }
