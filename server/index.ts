2d141f7bc26a6b7669183171eb4caa865aafe4be
Alex Harnett <harnettalex@gmail.com>
2025-05-11 15:17:44 -0700

    Initial api express route setup with simple text

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,23 @@ app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
+// Get all tubs
+app.get('/api/tubs', (req, res) => {
+  console.log('getting tubs')
+  res.send('Get all tubs')
+});
+
+// Creates a new tub
+app.post('/api/tubs', (req, res) => {
+  console.log('creating a new tub')
+});
+
+// Get all requests in a specific tub
+app.get('/api/tubs/:id', (req, res) => {
+  let publicId = req.params.id 
+  console.log(`getting all request for tub ${publicId}`)
+  res.send(`All requests from tub ${publicId}`)
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
