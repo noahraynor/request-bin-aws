e077f3749a60a8be64fb0a8c7712d7c1afa1fa11
Joshua Cutts <josh.cutts1@gmail.com>
2025-05-10 22:03:47 -0700

    added RequestHeader and Request components for the requests view. Modified the requests json to add the encoded_id as a property. added showHomepage const to toggle between displaying homepage and requests page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import tubsData from './data/tubs.json'
+import requestsData from './data/requests.json'
+
+const showHomepage = false
 
 function NewTub() {
   return (
@@ -27,12 +30,50 @@ function MyTubs() {
   )
 }
 
+function RequestHeader() {
+  const encoded_id = requestsData.encoded_id
+  const requestsLength = requestsData.requests.length
+  return (
+    <div>
+      <h1>{`Tub: ${encoded_id}`}</h1>
+      <p>Requests are collected at https://host/{encoded_id}</p>
+      <span>Requests Number: {requestsLength}</span>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  )
+}
+
+function Request({request}) {
+
+    return (
+        <div>
+            <p>METHOD: {request.method}</p>
+            <p>TIME: {request.time}</p>
+            <p>DATE: {request.date}</p>
+            <p>HEADERS</p>
+            <p>{JSON.stringify(request.headers)}</p>
+            <p>BODY</p>
+            <p>{JSON.stringify(request.body)}</p>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+        </div>
+        
+    )
+}
 
 function App() {
+  const requests = requestsData.requests
+
   return (
     <>
-      <NewTub />
-      <MyTubs />
+      {showHomepage && <NewTub />}
+      {showHomepage && <MyTubs />}
+      {!showHomepage && <RequestHeader />}
+      {!showHomepage && requests.map(request => <Request key={request.request_id} request={request}/>)}
     </>
   )
 }
