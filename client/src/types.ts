9c90c7de23f1cd63b19c2caa906f1e0c00a40de7
Alex Harnett <harnettalex@gmail.com>
2025-05-12 07:38:08 -0700

    Added types file to client

---
+++ client/src/types.ts
@@ -0,0 +1,12 @@
+export interface Tub {
+  id: Number,
+  encoded_id: String
+}
+
+export interface Request {
+  id: Number,
+  method: String,
+  headers: String,
+  Timestamp: String,
+  request_body: String
+}
