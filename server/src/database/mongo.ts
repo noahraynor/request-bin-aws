067924b985d25c0c869dc396e95cd9a8eeb4a636
Joshua Cutts <josh.cutts1@gmail.com>
2025-05-21 13:34:16 -0700

    updated mongo.ts to accomodate documentdb requirements

--- server/src/database/mongo.ts
+++ server/src/database/mongo.ts
@@ -1,10 +1,21 @@
 // server/src/db/mongo.ts
 import { MongoClient } from 'mongodb';
 import * as dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
-const client = new MongoClient(process.env.MONGO_URI!);
+// Construct full connection URI with TLS and retryWrites disabled
+const uri = `${process.env.MONGO_URI}/${process.env.MONGO_DB}?tls=true&retryWrites=false`;
+
+// Load Amazon DocumentDB's TLS CA bundle
+const caFilePath = path.resolve('/home/ssm-user/global-bundle.pem');
+
+const client = new MongoClient(uri, {
+  tls: true,
+  tlsCAFile: caFilePath,
+});
+
 const db = client.db(process.env.MONGO_DB);
 
 export { client, db };
