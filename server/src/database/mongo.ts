cc680daaa6833a34adc2291b0024934c3b29b146
Joshua Cutts <josh.cutts1@gmail.com>
2025-05-21 17:05:16 -0700

    Revert "Add js to relative imports"

--- server/src/database/mongo.ts
+++ server/src/database/mongo.ts
@@ -1,58 +1,10 @@
-import { MongoClient, Db } from 'mongodb';
-import path from 'path';
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from '@aws-sdk/client-secrets-manager';
-import {
-  SSMClient,
-  GetParametersByPathCommand,
-} from '@aws-sdk/client-ssm';
+// server/src/db/mongo.ts
+import { MongoClient } from 'mongodb';
+import * as dotenv from 'dotenv';
 
-const secretsClient = new SecretsManagerClient({});
-const ssmClient = new SSMClient({});
-const caFilePath = path.resolve('/home/ssm-user/global-bundle.pem');
+dotenv.config();
 
-let cachedDb: Promise<Db> | null = null;
+const client = new MongoClient(process.env.MONGO_URI!);
+const db = client.db(process.env.MONGO_DB);
 
-async function connectToMongo(): Promise<Db> {
-  // 1. Get secrets
-  const secretRes = await secretsClient.send(
-    new GetSecretValueCommand({ SecretId: 'request-tubs/db-credentials' })
-  );
-  const secrets = JSON.parse(secretRes.SecretString!);
-  const username = secrets.MONGO_USERNAME;
-  const password = secrets.MONGO_PASSWORD;
-
-  // 2. Get SSM params
-  const paramRes = await ssmClient.send(
-    new GetParametersByPathCommand({
-      Path: '/request-tubs',
-      WithDecryption: true,
-    })
-  );
-
-  const paramMap = Object.fromEntries(
-    paramRes.Parameters!.map(p => [p.Name!.split('/').pop()!, p.Value])
-  );
-
-  const uri = `${paramMap.MONGO_URI}/${paramMap.MONGO_DB}?tls=true&retryWrites=false`;
-
-  const client = new MongoClient(uri, {
-    auth: { username, password },
-    authMechanism: 'SCRAM-SHA-1',
-    tls: true,
-    tlsCAFile: caFilePath,
-  });
-
-  await client.connect();
-  return client.db(paramMap.MONGO_DB);
-}
-
-
-export const db: Promise<Db> = (async () => {
-  if (!cachedDb) {
-    cachedDb = connectToMongo();
-  }
-  return cachedDb;
-})();
+export { client, db };
